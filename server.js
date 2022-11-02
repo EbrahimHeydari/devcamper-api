687dacb8b738746ffa5e970e49a12b3738622b30
Ebrahim <akbrly42@gmail.com>
2022-11-02 16:53:28 +0330

    4.Getting Started With MongoDB  Bootcamps Resource

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const connectDB = require('./config/db')
+const colors = require('colors')
 
+// Load env vars
 dotenv.config({ path: './config/config.env' })
+const PORT = process.env.PORT || 4000
+const NODE_ENV = process.env.NODE_ENV
+
+// Connect to database
+connectDB()
 
 // Route files
 const bootcamps = require('./routes/bootcamps')
 
-// Load env vars
-const PORT = process.env.PORT || 4000
-const NODE_ENV = process.env.NODE_ENV
-
 const app = express()
+
+// Body Parser
+app.use(express.json())
 
 // Dev Logging midlleware
 if (NODE_ENV === 'development')
@@ -20,7 +27,15 @@ if (NODE_ENV === 'development')
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps)
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`server is running in ${NODE_ENV} mode on port ${PORT}`)
+  console.log(`server is running in ${NODE_ENV} mode on port ${PORT}`.yellow.bold)
 )
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (error, promise) => {
+  console.log(`Error: ${error.message}`.red)
+
+  // Close server & exit process
+  server.close(() => process.exit(1))
+})
