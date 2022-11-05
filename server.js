b08d4724ecd35ff89f40deac27cf99076d8d4b5d
Ebrahim <akbrly42@gmail.com>
2022-11-05 17:23:53 +0330

    5. Custom Error Handling  Mongoose Middleware

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv')
 const morgan = require('morgan')
 const connectDB = require('./config/db')
 const colors = require('colors')
+const errorHandler = require('./middleware/error')
 
 // Load env vars
 dotenv.config({ path: './config/config.env' })
@@ -26,6 +27,8 @@ if (NODE_ENV === 'development')
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps)
+
+app.use(errorHandler)
 
 const server = app.listen(
   PORT,
