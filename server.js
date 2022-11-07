8830cb934ee1868985867cffa779699c7da7babc
Ebrahim <akbrly42@gmail.com>
2022-11-07 19:16:51 +0330

    6. Mongoose Advanced Querying  Relationships

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const connectDB = require('./config/db')
 const colors = require('colors')
 const errorHandler = require('./middleware/error')
+const fileUpload = require('express-fileupload')
 
 // Load env vars
 dotenv.config({ path: './config/config.env' })
@@ -15,6 +17,7 @@ connectDB()
 
 // Route files
 const bootcamps = require('./routes/bootcamps')
+const courses = require('./routes/courses')
 
 const app = express()
 
@@ -25,8 +28,15 @@ app.use(express.json())
 if (NODE_ENV === 'development')
   app.use(morgan('dev'))
 
+// File uploading
+app.use(fileUpload())
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps)
+app.use('/api/v1/courses', courses)
 
 app.use(errorHandler)
 
